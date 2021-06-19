7b62accaea53f8cf96d990a56ed5d8f2c695d151
Jesse <jessemutua76@gmail.com>
2021-06-19 11:44:06 +0300

    Add comtent to body tag

---
+++ js/scripts.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    $("#development-icon").click(function() {
+        $("#development-icon").slideDown('1500').hide('1000')
+        $("#development-icon").show('1500')
+    })
+})
