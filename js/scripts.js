367673140dd17b19bd231fb13d6eb190b82d93a2
Jesse <jessemutua76@gmail.com>
2021-06-21 16:16:57 +0300

    Make final changes

--- js/scripts.js
+++ js/scripts.js
@@ -1,104 +1,42 @@
-// $(document).ready(function() {
-// $("#development-icon").click(function() {
-//     $(".development").slideDown('1500').hide('1000')
-//     $("#par2").show('1500')
-// });
-// $("#par2").click(function() {
-//     $("#par2").slideUp('1500');
-//     $("#development-icon").show('1500')
-// });
-// });
+function dev(i, p) {
 
-$(document).ready(function() {
-    $("#development-icon").click(function() {
-        $("this").children(".develoment").slideToggle(1300);
-        $("this").children("show").slideToggle(1400);
-
+    $(document).ready(function() {
+        i.click(function() {
+            i.toggle();
+            p.toggle();
+        });
+        p.click(function() {
+            i.toggle();
+            p.toggle();
+        });
     });
-    // event.preventDefault();
-
-});
-
-
-
-
-
-// $(document).ready(function() {
-//     $("#product-icon").click(function() {
-//         $("#product-icon").slideDown('1500').hide('1000');
-//         $("#product").show('1500');
-//     });
-//     $("#product").click(function() {
-//         $("#product").slideUp('1500');
-//         $("#product-icon").slideDown('1500');
-//     });
-// });
-
-// $(document).ready(function() {
-//     $("#design-icon").click(function() {
-//         $("#design-icon").slideDown('1500').hide('1000');
-//         $("#design").show('1500');
-//     });
-//     $("#product").click(function() {
-//         $("#product").slideUp('1500');
-//         $("product-icon").slideDown('1500');
-//     })
-// });
+}
+dev($("#mobile"), $("#par1"));
+dev($("#mobile2"), $("#par2"));
+dev($("#mobile3"), $("#par3"));
 
 // $(document).ready(function() {
 //     $("#work1").mouseover(function() {
-//         $("#overlay").show();
+//         $("#overlay1").show();
 //     }).mouseout(function() {
-//         $("#overlay").hide();
+//         $("#overlay1").hide();
 //     });
-// });
+// // });
+// $("#work1").hover(function() {
+//     $("this").children("overlay1").slideToggle(1000, "linear")
+// })
 
-// $(document).ready(function() {
-//     $("#work2").mouseover(function() {
-//         $("#overlay2").show();
-//     }).mouseout(function() {
-//         $("#overlay2").hide();
-//     });
-// });
-// $(document).ready(function() {
-//     $("#work3").mouseover(function() {
-//         $("#overlay3").show();
-//     }).mouseout(function() {
-//         $("#overlay3").hide();
-//     });
-// });
-// $(document).ready(function() {
-//     $("#work4").mouseover(function() {
-//         $("#overlay4").show();
-//     }).mouseout(function() {
-//         $("#overlay4").hide();
-//     });
-// });
-// $(document).ready(function() {
-//     $("#work5").mouseover(function() {
-//         $("#overlay5").show();
-//     }).mouseout(function() {
-//         $("#overlay5").hide();
-//     });
-// });
-// $(document).ready(function() {
-//     $("#work6").mouseover(function() {
-//         $("#overlay6").show();
-//     }).mouseout(function() {
-//         $("#overlay6").hide();
-//     });
-// });
-// $(document).ready(function() {
-//     $("#work7").mouseover(function() {
-//         $("#overlay7").show();
-//     }).mouseout(function() {
-//         $("#overlay7").hide();
-//     });
-// });
-// $(document).ready(function() {
-//     $("#work8").mouseover(function() {
-//         $("#overlay8").show();
-//     }).mouseout(function() {
-//         $("#overlay8").hide();
-//     });
-// });
+
+$(document).ready(function() {
+    $("#123").submit(function(event) {
+        // event.preventDefault();
+        var name = $("input#MERGE1").val();
+        var email = $("input#MERGE0").val();
+        var message = $("textarea#comment").val();
+        if ($("input#MERGE1").val() && $("input#MERGE0").val()) {
+            alert(name + ", we have received your message. Thank you for reaching out to us.");
+        } else {
+            alert("Please enter your name and email!");
+        }
+    });
+});
